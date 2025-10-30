62005bf780b155131ea7373712926c5afc258873
gommdamm <goyoudam2357@gmail.com>
2025-10-30 15:46:08 +0900

    09-2

--- main.c
+++ main.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define SIZE 5 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) 
 {
-	int i;
-	int score[5];
+	int i,ave;
+	int sum;
+	int score[SIZE];
 	
-	score[0]=10;
-	score[1]=20;
-	score[2]=30;
-	score[3]=40;
-	score[4]=50;
-	
-	for (i=0;i<5;i++)
+	sum=0;
+	for(i=0;i<SIZE;i++)
 	{
-		printf("grade[%d] = %d\n", i, score[i]);
+		printf("score : ");
+		scanf("%d", &score[i]);
+		sum+=score[i]; 
 	}
+		
+	ave = sum / SIZE;
+	printf("ave : %d\n", ave);
 	
 	return 0;
 }
