8b07c0630e06f3ab152bcbf060096188db83ccd6
gommdamm <goyoudam2357@gmail.com>
2025-10-30 16:13:58 +0900

    09-4

--- main.c
+++ main.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5 
+#define SIZE 4
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void square_array(int a[], int size);
+void print_array(int a[], int size);
 
 int main(int argc, char *argv[]) 
 {
-	int i;
-	int a[SIZE]={1,2,3,4,5};
-	int b[SIZE]={1,2,3,4,8};
-	int flag_same=1;
- 
-	for(i=0;i<SIZE;i++)
-	{	
-		if (a[i] != b[i])
-		{
-			printf("%i is different\n", i);
-			flag_same=0;
-		}
-			
-	}
+	int list[SIZE]={1,2,3,4};
 	
-		
-	
-	printf("a and b same? : %i\n", flag_same);
-		
+	print_array(list, SIZE);
+	square_array(list, SIZE);
+	print_array(list,SIZE);	
 	
 	return 0;
 }
+
+void square_array(int a[], int size) //크기지정 안해도 됨. 
+{
+	int i;
+	
+	for(i=0;i<size;i++)
+		a[i]=a[i]*a[i];
+}
+
+void print_array(int a[], int size)
+{
+	int i;
+	
+	for(i=0;i<size;i++)
+		printf("%3d", a[i]);
+	printf("\n");
+}
+
+//대소문자 size 
